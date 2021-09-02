2471259a4111824bfd6d4476fb0ac09af4238cac
royqh1979@gmail.com <Roy>
2021-09-02 22:40:22 +0800

    add demo file for astyle code format options dialog

---
+++ RedPandaIDE/resources/formatdemo.cpp
@@ -0,0 +1,18 @@
+namespace asylte_test{
+	class ClassA{
+	public:
+		void voo();
+	private:
+		void foo();
+	};
+    int main() {
+        int x;
+        if (x>10){
+            for (int i=0;i<10;i++)
+                x+=i*20;
+        }
+        return 0;
+    }
+}
+
+
