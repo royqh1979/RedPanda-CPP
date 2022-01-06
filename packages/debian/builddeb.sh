4523e8017cd8af9dc160d917df52d4af54f66334
Roy Qu <roy@Roy-Debian.localdomain>
2022-01-06 16:24:03 +0800

    scripts for make debian source pacakge

---
+++ packages/debian/builddeb.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+TMP_FOLDER=/tmp/redpandaide
+mkdir $TMP_FOLDER
+
+cp -r packages/debian $TMP_FOLDER 
+cp -r astyle $TMP_FOLDER 
+cp -r consolepauser $TMP_FOLDER
+cp -r RedPandaIDE $TMP_FOLDER
+cp README.md $TMP_FOLDER
+cp LICENSE $TMP_FOLDER
+cp NEWS.md $TMP_FOLDER
+cp redpandaide.desktop.in $TMP_FOLDER
+cp -r templates $TMP_FOLDER
+cp Red_Panda_CPP.pro $TMP_FOLDER
+cp linux $TMP_FOLDER
+
+cd $TMP_FOLDER
+dpkg-buildpackage -us -uc
