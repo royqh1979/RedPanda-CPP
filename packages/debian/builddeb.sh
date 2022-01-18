183efb93c7a524146fb7311683928d92552fce44
Roy Qu <royqh@gmail.com>
2022-01-18 22:38:27 +0800

    fix debian package script

--- packages/debian/builddeb.sh
+++ packages/debian/builddeb.sh
@@ -3,7 +3,10 @@
 make distclean
 
 TMP_FOLDER=/tmp/redpandaide
-mkdir $TMP_FOLDER
+test -n $TMP_FOLDER | rm -rf $TMP_FOLDER
+test -z $TMP_FOLDER | mkdir $TMP_FOLDER
+
+
 
 cp -r packages/debian $TMP_FOLDER 
 cp -r astyle $TMP_FOLDER 
@@ -12,8 +15,7 @@ cp -r RedPandaIDE $TMP_FOLDER
 cp README.md $TMP_FOLDER
 cp LICENSE $TMP_FOLDER
 cp NEWS.md $TMP_FOLDER
-cp redpandaide.desktop.in $TMP_FOLDER
-cp -r templates $TMP_FOLDER
+cp -r linux $TMP_FOLDER
 cp Red_Panda_CPP.pro $TMP_FOLDER
 cp -r linux $TMP_FOLDER
 
