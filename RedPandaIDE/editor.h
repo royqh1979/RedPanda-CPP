2b94c752df072f53ea8cffd5c84c291f5e804429
Roy Qu <royqh@gmail.com>
2023-10-18 17:07:16 +0800

    disalbe code suggestion while using ibus as the input method

--- RedPandaIDE/editor.h
+++ RedPandaIDE/editor.h
@@ -355,6 +355,9 @@ private:
     QMap<QString,StatementKind> mIdentCache;
 
     static QHash<ParserLanguage,std::weak_ptr<CppParser>> mSharedParsers;
+#ifdef Q_OS_UNIX
+    static int mShouldDisableSuggestionInInputMethodEvent;
+#endif
 
     // QWidget interface
 protected:
