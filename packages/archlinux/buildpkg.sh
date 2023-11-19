5c810bd1c9cc1eba4fc002382c098db68f101323
Cyano Hao <c@cyano.cn>
2023-11-19 08:48:58 +0800

    Update Linux packaging (#160)
    * update arch linux deps; make it CI-friendly
    
    * fix musl compatibility
    
    * alpine linux packaging
    
    * add musl-based Linux to CI
    
    * update rpm build
    
    * update doc

---
+++ packages/archlinux/buildpkg.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -xeuo pipefail
+
+TMP_FOLDER=/tmp/redpanda-cpp-git
+[[ -d $TMP_FOLDER ]] && rm -rf $TMP_FOLDER
+mkdir -p "$TMP_FOLDER"
+
+VERSION=$(git describe --long --tags | sed 's/^v//;s/\([^-]*-g\)/r\1/;s/-/./g') || VERSION="0.0.r$(git rev-list HEAD --count).g$(git rev-parse --short HEAD)"
+sed "s/__VERSION__/$VERSION/g" packages/archlinux/PKGBUILD.in >"$TMP_FOLDER/PKGBUILD"
+
+git archive --prefix="RedPanda-CPP/" -o "$TMP_FOLDER/RedPanda-CPP.tar.gz" HEAD
+
+cd "$TMP_FOLDER"
+makepkg -s --noconfirm
--- packages/opensuse/buildrpm.sh
+++ packages/opensuse/buildrpm.sh
@@ -3,11 +3,14 @@
 set -xeuo pipefail
 
 SPECDIR=$(rpm --eval %{_specdir})
+SOURCEDIR=$(rpm --eval %{_sourcedir})
 BUILDDIR=$(rpm --eval %{_builddir})/redpanda-cpp-git
-VERSION=$(git describe --long --tags --always | sed 's/^v//;s/\([^-]*-g\)/r\1/;s/-/./g')
-
-sed -s "s/__VERSION__/$VERSION/g" packages/opensuse/redpanda-cpp-git.spec.in >$SPECDIR/redpanda-cpp-git.spec
-
 [[ -d $BUILDDIR ]] && rm -rf $BUILDDIR
 
-rpmbuild --define "_sourcedir $(pwd)" -bb $SPECDIR/redpanda-cpp-git.spec
+VERSION=$(git describe --long --tags | sed 's/^v//;s/\([^-]*-g\)/r\1/;s/-/./g') || VERSION="0.0.r$(git rev-list HEAD --count).g$(git rev-parse --short HEAD)"
+sed "s/__VERSION__/$VERSION/g" packages/opensuse/redpanda-cpp-git.spec.in >$SPECDIR/redpanda-cpp-git.spec
+
+git archive --prefix="RedPanda-CPP/" -o "$SOURCEDIR/RedPanda-CPP.tar.gz" HEAD
+
+sudo zypper in -y $(rpmspec -q --buildrequires $SPECDIR/redpanda-cpp-git.spec)
+rpmbuild --nodebuginfo -bb $SPECDIR/redpanda-cpp-git.spec
