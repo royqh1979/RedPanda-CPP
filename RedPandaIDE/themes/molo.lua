ea3aebde2e68dc5c588c9985b4811a37e98e61dc
Ltabsyy <111294086+Ltabsyy@users.noreply.github.com>
2024-01-02 10:27:20 +0800

    Add files via upload

---
+++ RedPandaIDE/themes/molo.lua
@@ -0,0 +1,43 @@
+local nameMap = {
+    en_US = "MoLo",
+    pt_BR = "Escuro como tinta",
+    zh_CN = "墨落主题",
+    zh_TW = "墨落主題"
+}
+
+local lang = C_Desktop.language()
+
+return {
+    ["name"] = nameMap[lang] or nameMap.en_US,
+    ["style"] = "RedPandaDarkFusion",
+    ["default scheme"] = "MoLo CodeBlack",
+    ["default iconset"] = "newlook",
+    ["palette"] = {
+        PaletteWindow = "#000000",
+        PaletteWindowText = "#FFFFFF",
+        PaletteBase = "#141414",
+        PaletteAlternateBase = "#191919",
+        PaletteButton = "#141414",
+        PaletteButtonDisabled = "#141414",
+        PaletteBrightText = "#ff0000",
+        PaletteText = "#FFFFFF",
+        PaletteButtonText = "#FFFFFF",
+        PaletteButtonTextDisabled = "#9DA9B5",
+        PaletteHighlight = "#aa1f75cc",
+        PaletteDark = "#232323",
+        PaletteHighlightedText = "#e7e7e7",
+        PaletteToolTipBase = "#66000000",
+        PaletteToolTipText = "#e7e7e7",
+        PaletteLink = "#007af4",
+        PaletteLinkVisited = "#a57aff",
+        PaletteWindowDisabled = "#0A0A0A",
+        PaletteWindowTextDisabled = "#9DA9B5",
+        PaletteHighlightDisabled = "#9DA9B5",
+        PaletteHighlightedTextDisabled = "#9DA9B5",
+        PaletteBaseDisabled = "#000000",
+        PaletteTextDisabled = "#9DA9B5",
+        PaletteMid = "#FFFFFF",
+        PaletteLight = "#505050",
+        PaletteMidLight = "#00ff00"
+    }
+}
