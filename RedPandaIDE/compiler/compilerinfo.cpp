277a036ad546d756325fdddd8cd842073612df0f
Cyano Hao <c@cyano.cn>
2024-01-06 16:01:17 +0800

    enable asan/hwasan option on all platforms for cross toolchain

--- RedPandaIDE/compiler/compilerinfo.cpp
+++ RedPandaIDE/compiler/compilerinfo.cpp
@@ -177,17 +177,13 @@ void CompilerInfo::prepareCompilerOptions()
     sl.append(QPair<QString,QString>("Strong","-strong"));
     sl.append(QPair<QString,QString>("All","-all"));
     addOption(CC_CMD_OPT_STACK_PROTECTOR , QObject::tr("Check for stack smashing attacks (-fstack-protector)"), groupName, false, false, true, "-fstack-protector", CompilerOptionType::Choice, sl);
-#if defined(Q_OS_UNIX)
     sl.clear();
     sl.append(QPair<QString,QString>("Address","address"));
-# ifdef __aarch64__
-    sl.append(QPair<QString,QString>("Hardware-assisted Address","hwaddress"));
-# endif
+    sl.append(QPair<QString,QString>("Hardware-assisted address (arm64 only)","hwaddress"));
     sl.append(QPair<QString,QString>("Thread","thread"));
     sl.append(QPair<QString,QString>("Leak","leak"));
     sl.append(QPair<QString,QString>("Undefined","undefined"));
     addOption(CC_CMD_OPT_ADDRESS_SANITIZER , QObject::tr("Enable Sanitizer (-fsanitize=)"), groupName, true, true, true, "-fsanitize=",CompilerOptionType::Choice,sl);
-#endif
 
     // Output
     //groupName = QObject::tr("Output");
