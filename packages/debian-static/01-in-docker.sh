d1b07f47b4059d0b248cb4e1573455c2b1f22f31
Cyano Hao <c@cyano.cn>
2024-03-05 12:11:40 +0800

    add statically linked package for Ubuntu 20.04

---
+++ packages/debian-static/01-in-docker.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+SRC_DIR="$PWD"
+
+# build RedPanda C++
+mkdir -p /build
+cd /build
+qmake PREFIX=/usr "$SRC_DIR/Red_Panda_CPP.pro"
+make LINUX_STATIC_IME_PLUGIN=ON -j$(nproc)
+
+# install RedPanda C++ to AppDir
+make INSTALL_ROOT=/out install
+# remove unnecessary, huge files
+rm /out/usr/libexec/RedPandaCPP/redpanda-git-askpass
