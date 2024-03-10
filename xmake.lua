0328a36893dc639f19c006eae0b946a59a22f070
Cyano Hao <c@cyano.cn>
2024-03-10 08:04:41 +0800

    fix wayland icon by matching desktop file with window class (#257)

--- xmake.lua
+++ xmake.lua
@@ -230,13 +230,13 @@ target("resources")
     -- desktop entry
 
     if is_xdg() then
-        add_configfiles("platform/linux/redpandaide.desktop.in", {
+        add_configfiles("platform/linux/RedPandaIDE.desktop.in", {
             pattern = "$${(.-)}",
             variables = {
                 PREFIX = get_config("prefix"),
             },
         })
-        add_installfiles("$(buildir)/redpandaide.desktop", {prefixdir = "$(prefix)/share/applications"})
+        add_installfiles("$(buildir)/RedPandaIDE.desktop", {prefixdir = "$(prefix)/share/applications"})
     end
 
     -- mime type
