100de8d45585bbd93ad7c11709b3dd9b6e164f23
Cyano Hao <c@cyano.cn>
2024-05-09 17:25:12 +0800

    update nt 5 build script

--- packages/msys/build-xp.sh
+++ packages/msys/build-xp.sh
@@ -5,11 +5,10 @@ set -euxo pipefail
 function fn_print_help() {
   cat <<EOF
 Usage:
-  packages/msys/build-xp.sh [-m|--msystem <MSYSTEM>] [-c|--clean] [-nd|--no-deps] [-t|--target-dir <dir>]
+  packages/msys/build-xp.sh -p|--profile <profile> [-c|--clean] [-nd|--no-deps] [-t|--target-dir <dir>]
 Options:
   -h, --help               Display this information.
-  -m, --msystem <MSYSTEM>  Switch to other MSYS2 environment.
-                           (MINGW32, MINGW64, UCRT64, CLANG32, CLANG64)
+  -p, --profile <profile>  MinGW Lite profile.
                            MUST be used before other options.
   -c, --clean              Clean build and package directories.
   --mingw                  Alias for --mingw32 (x86 app) or --mingw64 (x64 app).
@@ -18,7 +17,7 @@ Options:
   --ucrt <build>           Include UCRT in the package. Windows SDK required.
                            e.g. '--ucrt 22621' for Windows 11 SDK 22H2.
   --qt <dir>               Path to Qt library.
-                           Default: /c/Qt/${QT_VERSION}/${QT_NAME}.
+                           Default: /c/Qt/${QT_VERSION}/mingw141_\${PROFILE}-redpanda.
   -nd, --no-deps           Skip dependency check.
   -t, --target-dir <dir>   Set target directory for the packages.
 EOF
@@ -27,43 +26,37 @@ EOF
 source version.inc
 [[ -n "${APP_VERSION_SUFFIX}" ]] && APP_VERSION="${APP_VERSION}${APP_VERSION_SUFFIX}"
 
+if [[ $# -lt 2 || ($1 != "-p" && $1 != "--profile") ]]; then
+  echo "Missing profile argument."
+  exit 1
+fi
+
 if [[ ! -v MSYSTEM ]]; then
   echo "This script must be run in MSYS2 shell"
   exit 1
 fi
 
-if [[ $# -gt 1 && ($1 == "-m" || $1 == "--msystem") ]]; then
-  msystem=$2
-  shift 2
-  case "${msystem}" in
-    MINGW32|MINGW64|UCRT64|CLANG32|CLANG64)
-      export MSYSTEM="${msystem}"
-      exec /bin/bash --login "$0" "$@"
-      ;;
-    *)
-      echo "Invalid MSYSTEM: ${msystem}"
-      exit 1
-      ;;
-  esac
-fi
+PROFILE=$2
+shift 2
 
 QT_VERSION="5.15.13"
-QT_NAME=""
-case "${MSYSTEM}" in
-  MINGW32|CLANG32)  # there is no UCRT32
-    NSIS_ARCH=x86
-    PACKAGE_BASENAME="redpanda-cpp-${APP_VERSION}-winxp_x86"
-    QT_NAME="mingw132_32-redpanda"
-    ;;
-  MINGW64|UCRT64|CLANG64)
+QT_NAME="mingw141_${PROFILE}-redpanda"
+case "${PROFILE}" in
+  64-ucrt|64-msvcrt)
     NSIS_ARCH=x64
     PACKAGE_BASENAME="redpanda-cpp-${APP_VERSION}-ws2003_x64"
-    QT_NAME="mingw132_64-redpanda"
+    ;;
+  32-ucrt|32-msvcrt)
+    NSIS_ARCH=x86
+    PACKAGE_BASENAME="redpanda-cpp-${APP_VERSION}-winxp_x86"
+    ;;
+  32-win2000)
+    NSIS_ARCH=x86
+    PACKAGE_BASENAME="redpanda-cpp-${APP_VERSION}-win2000_x86"
     ;;
   *)
-    echo "This script must be run in one of the following MSYS2 shells:"
-    echo "  - MINGW32 / CLANG32"
-    echo "  - MINGW64 / UCRT64 / CLANG64"
+    echo "Invalid profile: ${PROFILE}"
+    echo "Available profiles: 64-ucrt, 32-ucrt, 64-msvcrt, 32-msvcrt, 32-win2000"
     exit 1
     ;;
 esac
@@ -73,6 +66,7 @@ CHECK_DEPS=1
 compilers=()
 COMPILER_MINGW32=0
 COMPILER_MINGW64=0
+COMPILER_MINGW32_WIN2000=0
 TARGET_DIR="$(pwd)/dist"
 UCRT=""
 QT_DIR="/c/Qt/${QT_VERSION}/${QT_NAME}"
@@ -87,23 +81,37 @@ while [[ $# -gt 0 ]]; do
       shift
       ;;
     --mingw)
-      case "${NSIS_ARCH}" in
-        x86)
+      case "${PROFILE}" in
+        64-ucrt|64-msvcrt)
+          compilers+=("mingw64")
+          COMPILER_MINGW64=1
+          shift
+          ;;
+        32-ucrt|32-msvcrt)
           compilers+=("mingw32")
           COMPILER_MINGW32=1
           shift
           ;;
-        x64)
-          compilers+=("mingw64")
-          COMPILER_MINGW64=1
+        32-win2000)
+          compilers+=("mingw32-win2000")
+          COMPILER_MINGW32_WIN2000=1
           shift
           ;;
       esac
       ;;
     --mingw32)
-      compilers+=("mingw32")
-      COMPILER_MINGW32=1
-      shift
+      case "${PROFILE}" in
+        64-ucrt|32-ucrt|64-msvcrt|32-msvcrt)
+          compilers+=("mingw32")
+          COMPILER_MINGW32=1
+          shift
+          ;;
+        32-win2000)
+          compilers+=("mingw32-win2000")
+          COMPILER_MINGW32_WIN2000=1
+          shift
+          ;;
+      esac
       ;;
     --mingw64)
       compilers+=("mingw64")
@@ -111,8 +119,16 @@ while [[ $# -gt 0 ]]; do
       shift
       ;;
     --ucrt)
-      UCRT="$2"
-      shift 2
+      case "${PROFILE}" in
+        64-ucrt|32-ucrt)
+          UCRT="$2"
+          shift 2
+          ;;
+        *)
+          echo "Error: Red Panda C++ is not built against UCRT."
+          exit 1
+          ;;
+      esac
       ;;
     --qt)
       QT_DIR="$2"
@@ -134,32 +150,36 @@ while [[ $# -gt 0 ]]; do
 done
 
 ASTYLE_VERSION_TAG="3.4.14"
-BUILD_DIR="${TEMP}/redpanda-xp-${NSIS_ARCH}-build"
+BUILD_DIR="${TEMP}/redpanda-xp-${PROFILE}-build"
 ASTYLE_BUILD_DIR="${BUILD_DIR}/astyle"
-PACKAGE_DIR="${TEMP}/redpanda-xp-${NSIS_ARCH}-pkg"
+PACKAGE_DIR="${TEMP}/redpanda-xp-${PROFILE}-pkg"
 QMAKE="${QT_DIR}/bin/qmake.exe"
 NSIS="/mingw32/bin/makensis"
+_7Z="/mingw64/bin/7z"
+CMAKE="/mingw64/bin/cmake"
 SOURCE_DIR="$(pwd)"
 ASSETS_DIR="${SOURCE_DIR}/assets"
 UCRT_DIR="/c/Program Files (x86)/Windows Kits/10/Redist/10.0.${UCRT}.0/ucrt/DLLs/${NSIS_ARCH}"
 
-MINGW_LITE_RELEASE="13.2.0-r4"
-HOST_MINGW32_ARCHIVE="mingw32-ucrt-${MINGW_LITE_RELEASE}.7z"
-HOST_MINGW64_ARCHIVE="mingw64-ucrt-${MINGW_LITE_RELEASE}.7z"
+MINGW_LITE_RELEASE="14.1.0-r1"
+HOST_MINGW_ARCHIVE="mingw${PROFILE}-${MINGW_LITE_RELEASE}.7z"
+HOST_MINGW_BIT="${PROFILE%%-*}"
 
 REDPANDA_MINGW_RELEASE="11.4.0-r0"
 MINGW32_ARCHIVE="mingw32-${REDPANDA_MINGW_RELEASE}.7z"
-MINGW32_COMPILER_NAME="MinGW-w64 i686 GCC 11.4"
-MINGW32_PACKAGE_SUFFIX="mingw32_11.4"
+MINGW32_COMPILER_NAME="MinGW-w64 i686 GCC"
+MINGW32_PACKAGE_SUFFIX="mingw32"
 
 MINGW64_ARCHIVE="mingw64-${REDPANDA_MINGW_RELEASE}.7z"
-MINGW64_COMPILER_NAME="MinGW-w64 x86_64 GCC 11.4"
-MINGW64_PACKAGE_SUFFIX="mingw64_11.4"
+MINGW64_COMPILER_NAME="MinGW-w64 x86_64 GCC"
+MINGW64_PACKAGE_SUFFIX="mingw64"
+
+MINGW32_WIN2000_ARCHIVE="mingw32-win2000-${MINGW_LITE_RELEASE}.7z"
 
 if [[ ${#compilers[@]} -eq 0 ]]; then
   PACKAGE_BASENAME="${PACKAGE_BASENAME}-none"
 else
-  [[ ${COMPILER_MINGW32} -eq 1 ]] && PACKAGE_BASENAME="${PACKAGE_BASENAME}-${MINGW32_PACKAGE_SUFFIX}"
+  [[ ${COMPILER_MINGW32} -eq 1 || ${COMPILER_MINGW32_WIN2000} -eq 1 ]] && PACKAGE_BASENAME="${PACKAGE_BASENAME}-${MINGW32_PACKAGE_SUFFIX}"
   [[ ${COMPILER_MINGW64} -eq 1 ]] && PACKAGE_BASENAME="${PACKAGE_BASENAME}-${MINGW64_PACKAGE_SUFFIX}"
 fi
 
@@ -176,7 +196,8 @@ if [[ ${CHECK_DEPS} -eq 1 ]]; then
     exit 1
   fi
   deps=(
-    ${MINGW_PACKAGE_PREFIX}-7zip
+    mingw-w64-x86_64-7zip
+    mingw-w64-x86_64-cmake
     mingw-w64-i686-nsis
     curl git
   )
@@ -218,21 +239,15 @@ fi
 if [[ ${COMPILER_MINGW64} -eq 1 && ! -f "${ASSETS_DIR}/${MINGW64_ARCHIVE}" ]]; then
   curl -L "https://github.com/redpanda-cpp/toolchain-win32-mingw-xp/releases/download/${REDPANDA_MINGW_RELEASE}/${MINGW64_ARCHIVE}" -o "${ASSETS_DIR}/${MINGW64_ARCHIVE}"
 fi
+if [[ ${COMPILER_MINGW32_WIN2000} -eq 1 && ! -f "${ASSETS_DIR}/${MINGW32_WIN2000_ARCHIVE}" ]]; then
+  curl -L "https://github.com/redpanda-cpp/mingw-lite/releases/download/${MINGW_LITE_RELEASE}/${MINGW32_WIN2000_ARCHIVE}" -o "${ASSETS_DIR}/${MINGW32_WIN2000_ARCHIVE}"
+fi
 
 ## prepare host compiler
 
-case "${NSIS_ARCH}" in
-  x86)
-    [[ -f "${ASSETS_DIR}/${HOST_MINGW32_ARCHIVE}" ]] || curl -L "https://github.com/redpanda-cpp/mingw-lite/releases/download/${MINGW_LITE_RELEASE}/${HOST_MINGW32_ARCHIVE}" -o "${ASSETS_DIR}/${HOST_MINGW32_ARCHIVE}"
-    [[ -x "${BUILD_DIR}/mingw32/bin/gcc.exe" ]] || 7z x "${ASSETS_DIR}/${HOST_MINGW32_ARCHIVE}" -o"${BUILD_DIR}"
-    export PATH="${BUILD_DIR}/mingw32/bin:${PATH}"
-    ;;
-  x64)
-    [[ -f "${ASSETS_DIR}/${HOST_MINGW64_ARCHIVE}" ]] || curl -L "https://github.com/redpanda-cpp/mingw-lite/releases/download/${MINGW_LITE_RELEASE}/${HOST_MINGW64_ARCHIVE}" -o "${ASSETS_DIR}/${HOST_MINGW64_ARCHIVE}"
-    [[ -x "${BUILD_DIR}/mingw64/bin/gcc.exe" ]] || 7z x "${ASSETS_DIR}/${HOST_MINGW64_ARCHIVE}" -o"${BUILD_DIR}"
-    export PATH="${BUILD_DIR}/mingw64/bin:${PATH}"
-    ;;
-esac
+[[ -f "${ASSETS_DIR}/${HOST_MINGW_ARCHIVE}" ]] || curl -L "https://github.com/redpanda-cpp/mingw-lite/releases/download/${MINGW_LITE_RELEASE}/${HOST_MINGW_ARCHIVE}" -o "${ASSETS_DIR}/${HOST_MINGW_ARCHIVE}"
+[[ -x "${BUILD_DIR}/mingw${HOST_MINGW_BIT}/bin/gcc.exe" ]] || "${_7Z}" x "${ASSETS_DIR}/${HOST_MINGW_ARCHIVE}" -o"${BUILD_DIR}"
+export PATH="${BUILD_DIR}/mingw${HOST_MINGW_BIT}/bin:${PATH}"
 
 ## build
 fn_print_progress "Building astyle..."
@@ -242,7 +257,7 @@ popd
 
 pushd .
 cd "${ASTYLE_BUILD_DIR}"
-cmake . -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=Release  -DCMAKE_EXE_LINKER_FLAGS="-static"
+"${CMAKE}" . -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=Release  -DCMAKE_EXE_LINKER_FLAGS="-static"
 mingw32-make -j$(nproc)
 cp AStyle/AStyle.exe "${PACKAGE_DIR}/astyle.exe"
 popd
@@ -285,27 +300,37 @@ nsis_flags=(
   -DMINGW32_COMPILER_NAME="${MINGW32_COMPILER_NAME}"
   -DMINGW64_COMPILER_NAME="${MINGW64_COMPILER_NAME}"
 )
-case $NSIS_ARCH in
-  x86)
-    nsis_flags+=(
-      -DREQUIRED_WINDOWS_BUILD=2600
-      -DREQUIRED_WINDOWS_NAME="Windows XP"
-    )
-    ;;
-  x64)
+case "${PROFILE}" in
+  64-ucrt|64-msvcrt)
     nsis_flags+=(
       -DREQUIRED_WINDOWS_BUILD=3790
       -DREQUIRED_WINDOWS_NAME="Windows Server 2003"
     )
     ;;
+  32-ucrt|32-msvcrt)
+    nsis_flags+=(
+      -DREQUIRED_WINDOWS_BUILD=2600
+      -DREQUIRED_WINDOWS_NAME="Windows XP"
+    )
+    ;;
+  32-win2000)
+    nsis_flags+=(
+      -DREQUIRED_WINDOWS_BUILD=2195
+      -DREQUIRED_WINDOWS_NAME="Windows XP"
+    )
+    ;;
 esac
 if [[ ${COMPILER_MINGW32} -eq 1 ]]; then
   nsis_flags+=(-DHAVE_MINGW32)
-  [[ -d "mingw32" ]] || 7z x "${SOURCE_DIR}/assets/${MINGW32_ARCHIVE}" -o"${PACKAGE_DIR}"
+  [[ -d "mingw32" ]] || "${_7Z}" x "${SOURCE_DIR}/assets/${MINGW32_ARCHIVE}" -o"${PACKAGE_DIR}"
 fi
 if [[ ${COMPILER_MINGW64} -eq 1 ]]; then
   nsis_flags+=(-DHAVE_MINGW64)
-  [[ -d "mingw64" ]] || 7z x "${SOURCE_DIR}/assets/${MINGW64_ARCHIVE}" -o"${PACKAGE_DIR}"
+  [[ -d "mingw64" ]] || "${_7Z}" x "${SOURCE_DIR}/assets/${MINGW64_ARCHIVE}" -o"${PACKAGE_DIR}"
+fi
+if [[ ${COMPILER_MINGW32_WIN2000} -eq 1 ]]; then
+  nsis_flags+=(-DHAVE_MINGW32)
+  [[ -d "mingw32" ]] || "${_7Z}" x "${SOURCE_DIR}/assets/${MINGW32_WIN2000_ARCHIVE}" -o"${PACKAGE_DIR}"
 fi
 if [[ -n "${UCRT}" ]]; then
   nsis_flags+=(-DHAVE_UCRT)
@@ -317,8 +342,8 @@ fi
 "${NSIS}" "${nsis_flags[@]}" redpanda.nsi
 
 fn_print_progress "Making Portable Package..."
-7z x "${SETUP_NAME}" -o"RedPanda-CPP" -xr'!$PLUGINSDIR' -x"!uninstall.exe"
-7z a -mmt -mx9 -ms=on -mqs=on -mf=BCJ2 "${PORTABLE_NAME}" "RedPanda-CPP"
+"${_7Z}" x "${SETUP_NAME}" -o"RedPanda-CPP" -xr'!$PLUGINSDIR' -x"!uninstall.exe"
+"${_7Z}" a -mmt -mx9 -ms=on -mqs=on -mf=BCJ2 "${PORTABLE_NAME}" "RedPanda-CPP"
 rm -rf "RedPanda-CPP"
 
 mv "${SETUP_NAME}" "${TARGET_DIR}"
