e2934072d158cc26ee2b10c0065f82c8aceefdd1
Cyano Hao <c@cyano.cn>
2024-06-10 09:19:15 +0800

    use qt resource system's recommended approach for translation files (#449)

--- RedPandaIDE/utils.h
+++ RedPandaIDE/utils.h
@@ -204,4 +204,9 @@ std::tuple<QString, QStringList, PNonExclusiveTemporaryFileOwner> wrapCommandFor
 
 std::tuple<QString, QStringList, PNonExclusiveTemporaryFileOwner> wrapCommandForTerminalEmulator(const QString &terminal, const QString &argsPattern, const QStringList &payloadArgsWithArgv0);
 
+struct ExternalResource {
+    ExternalResource();
+    ~ExternalResource();
+};
+
 #endif // UTILS_H
