041df6fef0bb1aa36c1b29177ac5d69956cc5333
Cyano Hao <c@cyano.cn>
2024-12-05 07:10:55 +0800

    Upgrade AppImage build environment to Qt 6.8.1 (#533)

--- packages/debian-static/builddeb.sh
+++ packages/debian-static/builddeb.sh
@@ -13,7 +13,7 @@ DEB_FILE="redpanda-cpp-bin_${VERSION}_amd64.deb"
 
 TMP_FOLDER="$(mktemp -d)"
 
-podman run -it --rm -v "$PWD:/mnt" -w /mnt -v "$TMP_FOLDER:/out" ghcr.io/redpanda-cpp/appimage-builder-x86_64:20241120.0 packages/debian-static/01-in-docker.sh
+podman run -it --rm -v "$PWD:/mnt" -w /mnt -v "$TMP_FOLDER:/out" ghcr.io/redpanda-cpp/appimage-builder-x86_64:20241204.0 packages/debian-static/01-in-docker.sh
 
 mkdir -p dist
 mkdir -m 755 -p "$TMP_FOLDER/DEBIAN"
