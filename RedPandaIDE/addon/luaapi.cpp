888f233ff6ee2b8383a23552aef99e8a32799f9b
Cyano Hao <c@cyano.cn>
2024-12-22 11:38:30 +0800

    Simplify Windows on ARM support (#553)
    - Deprecate Windows 10
      - Lack of usage; no test environment.
    - Remove ARM64EC ABI
      - Was added for uncertainty of add-on tech, and x64 IME support. Now add-on will based on scripting language, and IMEs have good support for ARM64.
    - Deprecate ARM32
      - Lack of usage; dropped by hardware, OS, and libraries.

--- RedPandaIDE/addon/luaapi.cpp
+++ RedPandaIDE/addon/luaapi.cpp
@@ -411,8 +411,6 @@ extern "C" int luaApi_System_supportedAppArchList(lua_State *L) noexcept {
             arches.insert("i386");
         if (pGetMachineTypeAttributes(IMAGE_FILE_MACHINE_AMD64, &result) == 0 && result & win32_flag_UserEnabled)
             arches.insert("x86_64");
-        if (pGetMachineTypeAttributes(IMAGE_FILE_MACHINE_ARMNT, &result) == 0 && result & win32_flag_UserEnabled)
-            arches.insert("arm");
         if (pGetMachineTypeAttributes(IMAGE_FILE_MACHINE_ARM64, &result) == 0 && result & win32_flag_UserEnabled)
             arches.insert("arm64");
     } else {
@@ -441,8 +439,6 @@ extern "C" int luaApi_System_supportedAppArchList(lua_State *L) noexcept {
             BOOL wow64SupportResult;
             if (pIsWow64GuestMachineSupported(IMAGE_FILE_MACHINE_I386, &wow64SupportResult) == S_OK && wow64SupportResult)
                 arches.insert("i386");
-            if (pIsWow64GuestMachineSupported(IMAGE_FILE_MACHINE_ARMNT, &wow64SupportResult) == S_OK && wow64SupportResult)
-                arches.insert("arm");
         } else {
             // legacy Windows, hardcode
             SYSTEM_INFO si;
--- docs/detailed-build-win-cn.md
+++ docs/detailed-build-win-cn.md
@@ -16,7 +16,8 @@ qmake 变量：
   - `OpenConsole.exe` 需要 Windows 10 1809 加入的 ConPTY 接口。
 
 关于 ARM 上的 Windows 的注记：
-- 小熊猫C++ 只能在 Windows 11 ARM64 上构建 ARM64 版，成品应该能在 Windows 10 ARM64 上运行（但没有测试过）。
+- 小熊猫C++ 只能在 Windows 11 ARM64 上构建 ARM64 版。
+  - 在 Windows 10 上运行不再受支持。安装程序假设 x64 仿真始终可用。（原生的 “带 LLVM MinGW 工具链的小熊猫C++” 应该能用。）
   - 不支持 ARM64EC（“仿真兼容”）主机，即不能用 ARM64EC 工具链构建小熊猫 C++。
   - （理论上）支持 ARM64EC 目标，也就是说，如果上游工具链支持 ARM64EC，那么小熊猫C++ 可以构建 ARM64EC 程序和库。
 - 随着 [Windows 11 Insider Preview Build 25905 弃用 ARM32](https://blogs.windows.com/windows-insider/2023/07/12/announcing-windows-11-insider-preview-build-25905/)，小熊猫 C++ 今后也不会添加 ARM32 支持了。
