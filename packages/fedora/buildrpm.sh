fa22a2f34f779a8352bba73924d3f991ff37e07e
Cyano Hao <c@cyano.cn>
2025-03-18 13:12:37 +0800

    Fix CI build
    - Remove old versions
    - Workaround Fedora sudo change

--- packages/fedora/buildrpm.sh
+++ packages/fedora/buildrpm.sh
@@ -7,10 +7,12 @@ SOURCEDIR=$(rpm --eval %{_sourcedir})
 BUILDDIR=$(rpm --eval %{_builddir})/redpanda-cpp-git
 [[ -d $BUILDDIR ]] && rm -rf $BUILDDIR
 
+(( EUID == 0 )) && SUDO="" || SUDO="sudo"
+
 VERSION=$(git describe --long --tags | sed 's/^v//;s/\([^-]*-g\)/r\1/;s/-/./g') || VERSION="0.0.r$(git rev-list HEAD --count).g$(git rev-parse --short HEAD)"
 sed "s/__VERSION__/$VERSION/g" packages/fedora/redpanda-cpp-git.spec.in >$SPECDIR/redpanda-cpp-git.spec
 
 git archive --prefix="RedPanda-CPP/" -o "$SOURCEDIR/RedPanda-CPP.tar.gz" HEAD
 
-sudo dnf builddep -y $SPECDIR/redpanda-cpp-git.spec
+$SUDO dnf builddep -y $SPECDIR/redpanda-cpp-git.spec
 rpmbuild --nodebuginfo -bb $SPECDIR/redpanda-cpp-git.spec
