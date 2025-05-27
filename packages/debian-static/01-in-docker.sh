79b94aa104b982066700ed8c7cedae439ab7960a
Cyano Hao <c@cyano.cn>
2025-05-27 15:19:39 +0800

    Fix debian static ldflags

--- packages/debian-static/01-in-docker.sh
+++ packages/debian-static/01-in-docker.sh
@@ -7,7 +7,7 @@ SRC_DIR="$PWD"
 # build RedPanda C++
 mkdir -p /build
 cd /build
-qmake PREFIX=/usr "$SRC_DIR/Red_Panda_CPP.pro"
+qmake PREFIX=/usr QMAKE_LFLAGS=-static "$SRC_DIR/Red_Panda_CPP.pro"
 make -j$(nproc)
 
 # install RedPanda C++ to AppDir
