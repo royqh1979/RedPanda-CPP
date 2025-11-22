e486116a14c9788d864d2e9a0c77c76e54ece936
Roy Qu <royqh1979@gmail.com>
2025-11-22 21:44:53 +0800

    fix: spaces added by toggle block comment is not correctly toggle again.

--- libs/qsynedit/qsynedit/qsynedit.cpp
+++ libs/qsynedit/qsynedit/qsynedit.cpp
@@ -1602,9 +1602,9 @@ void QSynEdit::doToggleBlockComment()
         if (pos>=0) {
             newText.remove(pos,endLen);
         }
-        setSelText(newText);
+        setSelText(newText.trimmed());
     } else {
-        QString newText=QString("%1 %2 %3").arg(beginSymbol,selText(),endSymbol);
+        QString newText=QString("%1%2%3").arg(beginSymbol,selText(),endSymbol);
         setSelText(newText);
     }
 
