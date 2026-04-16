eb15fdeaedf0ebcdf4af4db750dcea927e7dfc13
Cyano Hao <c@cyano.cn>
2026-04-16 17:06:35 +0800

    Refarm MSYS2 to real MSYS2 package

---
+++ packages/msys/buildpkg.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -xeuo pipefail
+
+TMP_FOLDER=/tmp/redpanda-cpp
+[[ -d $TMP_FOLDER ]] && rm -rf $TMP_FOLDER
+mkdir -p "$TMP_FOLDER"
+
+# git describe: v3.4-56-g789abcd
+# sed: 3.4.r56.g789abcd
+#   - remove leading 'v'
+#   - prepend 'r' to the number before the '-g'
+#   - replace '-' with '.'
+# fallback: 0.0.r3456.g789abcd
+VERSION=$(git describe --long --tags | sed 's/^v//;s/\([^-]*-g\)/r\1/;s/-/./g') || VERSION="0.0.r$(git rev-list HEAD --count).g$(git rev-parse --short HEAD)"
+sed "s/__VERSION__/$VERSION/g" packages/msys/PKGBUILD.in >"$TMP_FOLDER/PKGBUILD"
+
+git archive --prefix="RedPanda-CPP/" -o "$TMP_FOLDER/RedPanda-CPP.tar.gz" HEAD
+
+(
+  cd "$TMP_FOLDER"
+  makepkg-mingw -s --noconfirm
+)
+
+mkdir -p dist
+cp /tmp/redpanda-cpp/mingw-w64-*-redpanda-cpp-*.pkg.tar.zst dist/
